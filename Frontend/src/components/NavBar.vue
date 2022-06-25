c903ddcd51a0f53fe619789b9396c46c6372aedf
Nícolas Farfán Cheneaux <nicolas.farfan.ch@usach.cl>
2022-06-24 20:49:13 -0400

    [fix] logo size fixed

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -1,5 +1,10 @@
 <style>@import '../assets/navBar.css';</style>
 <template>
+<div>
+  <div class="colorBar">
+    <div class="blue"></div>
+    <div class="orange"></div>
+  </div>
 <nav id="nb" class="navbar navbar-expand-lg bg-light">
 <div class="container-fluid">
 <ul class="navbar-nav mb-2 mb-lg-0">
@@ -15,4 +20,25 @@
 </ul>
 </div>
 </nav>
+</div>
 </template>
+<style scoped>
+#nb{
+  background-color: #f5f5f5;
+  border-bottom: 1px solid #e5e5e5;
+  border-top: 1px solid #e5e5e5;
+  padding: 0;
+  margin: 0;
+}
+.colorBar{
+  display: flex;
+}
+.blue{
+  width: 100%;
+  background-color: blue;
+}
+.orange{
+  width: 100%;
+  background-color: orange;
+}
+</style>
